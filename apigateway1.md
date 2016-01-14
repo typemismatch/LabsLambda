3cf35716e48a5111460ec9ad30302f89f21ac7c3
Craig Williams <craiwill@amazon.com>
2016-01-14 09:54:22 -0500

    re-org files

--- apigateway1.md
+++
@@ -1,3 +0,0 @@
-# Setup a REST API
-
-This REST API will use the API Gateway and is a great entry point to our Lambda functions.
--- lambda1.js
+++
@@ -1,8 +0,0 @@
-console.log('Loading function');
-
-exports.handler = function(event, context) {
-    // Let's log everything passed into this function via the event object.
-    console.log('Received event:', JSON.stringify(event));
-    if (event.Test == 'fail') context.fail("oops");
-    context.succeed(true);  // a lazy IF statement, return true except if the passed in eventTest = fail.
-};
